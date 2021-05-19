e371a77680cf602fe5dc67bed09c63fe5ab4e212
Halfus <47626287+Halfus@users.noreply.github.com>
2021-05-19 17:31:43 +0200

    both movements

--- Assignment 3 - Template/js/script.js
+++ Assignment 3 - Template/js/script.js
@@ -1,4 +1,27 @@
-//Getting a random value.
+//GETTING A RANDOM VALUE
+//Math.random() returns a value between range [0,1), max and min are changing that range [min, max).
+//First it multiplies the difference between max and min by a number between range [0,1)
+//so it's never more than the difference between max and min, and then it adds min.
+//Then returns an int.
 function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 };
+
+//BOTH MOVEMENTS
+//1. Slowly move around in random directions.
+function moveAround(id, speed)
+{
+    var x = getRandom(0, $(document).width() - 150);
+    var y = getRandom(0, $(document).height() - 150);
+
+    $(id).animate({top: y, left: x}, speed, function() {fishMoves(id, speed)} );
+};
+
+$(document).ready(function () {
+    fishMoves("#fish1Id", 6100)
+});
+
+$(document).ready(function () {
+    fishMoves("#fish2Id", 6500)
+});
+
