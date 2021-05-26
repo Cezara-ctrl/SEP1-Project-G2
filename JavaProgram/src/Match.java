af5eb805ff2ee729b215990f7d8e1984f7a70ed2
Halfus <47626287+Halfus@users.noreply.github.com>
2021-05-27 01:20:53 +0200

    Match

--- JavaProgram/src/Match.java
+++ JavaProgram/src/Match.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Match
 {
   private Date date;
   private Time time;
   private String place;
   private String type;
+  private Team team;
 
   public Match(Date date, Time time, String place, String type)
   {
@@ -31,5 +34,39 @@ public class Match
   public String getType()
   {
     return type;
+  }
+
+  public boolean hasValidDate()
+  {
+    boolean valDate = true;
+    if(date.getDate().isBefore(Date.today()))
+    {
+      valDate = false;
+    }
+    return valDate;
+  }
+
+  public boolean hasValidType()
+  {
+    boolean valType = false;
+    if(type == "League" || type == "Cup" || type == "Friendly")
+    {
+      valType = true;
+    }
+    return valType;
+  }
+
+  public boolean hasValidPlayersForType()
+  {
+
+  }
+
+  public void addTeam(Team t)
+  {
+    team = t;
+  }
+
+  public String toString(){
+    return type + " match on " + date + " " + time + " at " + place + " .\n" + "The team fot this match is: " + team;
   }
 }
