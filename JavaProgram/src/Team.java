739fb9b7daaf56d849b871e55ae00fb5d19c0559
IonCanariov <ioncnnn@gmail.com>
2021-05-26 20:33:06 +0200

    This is the team class.

--- JavaProgram/src/Team.java
+++ JavaProgram/src/Team.java
@@ -1,3 +1,32 @@
-public class Team {
+import java.util.ArrayList;
 
+public class Team {
+private ArrayList<Player> players;
+private int numberOfPlayers ;
+
+public Team(ArrayList<Player> players ,int numberOfPlayers){
+    this.numberOfPlayers=numberOfPlayers;
+    players=new ArrayList<Player>();
+}
+public void addPlayer(Player player){
+    players.add(player);
+}
+public void removePlayer(Player player){
+    players.remove(player);
+}
+public int getNumberOfPlayers(){
+    return players.size;
+}
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Team team = (Team) obj;
+        return players==team.players && numberOfPlayers==team.numberOfPlayers;
+    }
+    public String toString(){
+    return "Players"+players+"Number of the players"+numberOfPlayers;
+    }
 }
