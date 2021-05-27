8e60ea1a9c0d679017b3a90bdfeac311c2a84670
Rodrigo Valente <rodrigo.valente.2002@gmail.com>
2021-05-27 08:41:23 +0100

    Date class

--- JavaProgram/src/Team.java
+++ JavaProgram/src/Team.java
@@ -1,22 +1,31 @@
 import java.util.ArrayList;
 
-public class Team {
-private ArrayList<Player> players;
-private int numberOfPlayers ;
+public class Team
+{
+    private ArrayList<Player> players;
+    private int numberOfPlayers ;
 
-public Team(ArrayList<Player> players ,int numberOfPlayers){
-    this.numberOfPlayers=numberOfPlayers;
-    players=new ArrayList<Player>();
-}
-public void addPlayer(Player player){
-    players.add(player);
-}
-public void removePlayer(Player player){
-    players.remove(player);
-}
-public int getNumberOfPlayers(){
-    return players.size();
-}
+    public Team(ArrayList<Player> players ,int numberOfPlayers)
+    {
+        this.numberOfPlayers=numberOfPlayers;
+        players=new ArrayList<Player>();
+    }
+
+    public void addPlayer(Player player)
+    {
+        players.add(player);
+    }
+
+    public void removePlayer(Player player)
+    {
+        players.remove(player);
+    }
+
+    public int getNumberOfPlayers()
+    {
+        return players.size();
+    }
+
     public boolean equals(Object obj)
     {
         if (this == obj)
@@ -26,7 +35,10 @@ public int getNumberOfPlayers(){
         Team team = (Team) obj;
         return players==team.players && numberOfPlayers==team.numberOfPlayers;
     }
-    public String toString(){
-    return "Players"+players+"Number of the players"+numberOfPlayers;
+
+    public String toString()
+    {
+        return "Players"+players+"Number of the players"+numberOfPlayers;
     }
+
 }
