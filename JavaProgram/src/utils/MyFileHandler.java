02172ec13d929a37a235c3da3f7ab9c8d8bae0db
Rodrigo Valente <rodrigo.valente.2002@gmail.com>
2021-06-03 23:58:21 +0100

    i added comments for javadoc

--- JavaProgram/src/utils/MyFileHandler.java
+++ JavaProgram/src/utils/MyFileHandler.java
@@ -5,8 +5,19 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Class used for writing and reading binary files and text files
+ * @author Rodrigo Valente
+ * @version 1.0
+ */
 public class MyFileHandler
 {
+  /**
+   * Writes an object to a binary file
+   * @param fileName the name of the file in which the object will be written
+   * @param object the object that will be written in the specified file
+   * @throws IOException exception thrown if an IO error occurs
+   */
   public static void writeToBinaryFile(String fileName, Object object) throws IOException
   {
     ObjectOutputStream objectOutputStream = null;
@@ -34,7 +45,13 @@ public class MyFileHandler
     }
   }
 
-
+  /**
+   * Reads an object from a binary file
+   * @param fileName the name of the file from which the object will be read
+   * @return the object read from the specified file
+   * @throws IOException exception thrown if an IO error occurs
+   * @throws ClassNotFoundException exception thrown when no definition for the specified class could be found
+   */
   public static Object readFromBinaryFile(String fileName) throws IOException, ClassNotFoundException
   {
     Object object = null;
@@ -93,6 +110,12 @@ public class MyFileHandler
     }
   }
 
+  /**
+   * Writes a string to a text file
+   * @param fileName the name of the file in which the string will be written
+   * @param str the string that will be written in the specified file
+   * @throws FileNotFoundException exception thrown if the file could not be found
+   */
   public void writeToTextFile(String fileName, String str) throws FileNotFoundException
   {
     FileOutputStream fileOut = new FileOutputStream(fileName);
@@ -102,6 +125,12 @@ public class MyFileHandler
     System.out.println("Done writing to file.");
   }
 
+  /**
+   * Appends a string to a text file
+   * @param fileName the name of the file in which the string will be appended
+   * @param str the string that will be appended to the specified file
+   * @throws FileNotFoundException exception thrown if the file could not be found
+   */
   public void appendToTextFile(String fileName, String str) throws FileNotFoundException
   {
     FileOutputStream fileOut = new FileOutputStream(fileName, true);
@@ -111,6 +140,12 @@ public class MyFileHandler
     System.out.println("Done appending to file.");
   }
 
+  /**
+   * Reads a string from a text file
+   * @param fileName the name of the file from which the string will be read
+   * @return the read string
+   * @throws FileNotFoundException exception thrown if the file could not be found
+   */
   public String readFromTextFile(String fileName) throws FileNotFoundException
   {
     FileInputStream fileIn = new FileInputStream(fileName);
@@ -120,6 +155,12 @@ public class MyFileHandler
     return read.nextLine();
   }
 
+  /**
+   * Writes an array of strings to a text file
+   * @param fileName the name of the file in which the strings will be written
+   * @param string the array of strings that will be written in the specified file
+   * @throws FileNotFoundException exception thrown if the file could not be found
+   */
   public void writeArrayToTextFile(String fileName, String[] string) throws FileNotFoundException
   {
     FileOutputStream fileOut = new FileOutputStream(fileName);
@@ -132,6 +173,12 @@ public class MyFileHandler
     System.out.println("Done writing to file.");
   }
 
+  /**
+   * Appends an array of strings to a text file
+   * @param fileName the name of the file to which the strings will be appended
+   * @param strings the array of strings that will be appended
+   * @throws FileNotFoundException exception thrown if the file could not be found
+   */
   public void appendArrayToTextFile(String fileName, String[] strings) throws FileNotFoundException
   {
     FileOutputStream fileOut = new FileOutputStream(fileName, true);
@@ -144,6 +191,12 @@ public class MyFileHandler
     System.out.println("Done appending to file.");
   }
 
+  /**
+   * Reads an array of string from a text file
+   * @param fileName the name of the file from which the strings will be read
+   * @return the array of strings read from the specified file
+   * @throws FileNotFoundException exception thrown if the file could not be found
+   */
   public static String[] readArrayFromTextFile(String fileName) throws FileNotFoundException
   {
     Scanner readFromFile = null;
